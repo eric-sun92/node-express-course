8e3c0fa9241355d364c6195e49069ca26cfc1354
Eric <eric.sun.ecs92@yale.edu>
2022-08-07 23:53:30 -0400

    'task manager route setup'

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(3000, () => console.log("server running"));
---
+++ 03-task-manager/starter/db/connect.js
@@ -0,0 +1,2 @@
+const connectionString =
+  "mongodb+srv://ecs92:soccerpro47@cluster0.frdhqfz.mongodb.net/?retryWrites=true&w=majority";

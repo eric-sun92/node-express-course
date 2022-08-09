7672af0d11e18552cd4548c801e129851cee2b65
Eric <eric.sun.ecs92@yale.edu>
2022-08-08 23:54:45 -0400

    'finished task manager'

--- 03-task-manager/final/routes/tasks.js
+++ 03-task-manager/final/routes/tasks.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 const {
   getAllTasks,
@@ -7,10 +7,9 @@ const {
   getTask,
   updateTask,
   deleteTask,
-  editTask,
-} = require('../controllers/tasks')
+} = require("../controllers/tasks");
 
-router.route('/').get(getAllTasks).post(createTask)
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
-module.exports = router
+module.exports = router;
--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,17 +1,36 @@
+const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
+// const methodOverride = require("method-override");
+
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+
+require("dotenv").config();
+
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
-
+app.use(express.static("./public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride("_methodOverride"));
 
 //routes
-app.get("/hello", (req, res) => {
-  res.send("Task manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
 
-app.listen(3000, () => console.log("server running"));
+app.use(notFound);
+
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.DB_URL);
+    app.listen(PORT, () => console.log("server running"));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
